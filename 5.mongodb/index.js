4ae010500c3950e93b97f0bbdfaa2662ec38f4d8
JoseUH <garcia.arencon@bootcamp-upgrade.com>
2023-10-10 11:11:12 +0200

    feat

---
+++ 5.mongodb/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const {connect} = require("./src/utils/database");
+
+const PORT = 5000;
+
+const app = express();
+connect();
+
+app.listen(PORT, ()=> console.log(`listening in the port http://localhost:${PORT}`))
+
